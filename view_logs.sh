050f39783410784983e6205dbc24d1367b4b9ae5
Heli Vachhani <64596822+helihub-dev@users.noreply.github.com>
2025-10-22 00:37:17 -0700

    Delete view_logs.sh

--- view_logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# View real-time logs from Docker container
-
-echo "📋 Viewing AxleWave Discovery logs..."
-echo "Press Ctrl+C to stop"
-echo ""
-
-docker-compose logs -f --tail=100 axlewave-discovery
